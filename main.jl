1352b4fe3c160c0a257701ce4849b520021f76cf
Thomas Staudt <tscode@posteo.net>
2020-12-02 10:57:38 +0100

    solutions for day 2

--- main.jl
+++ main.jl
@@ -1,8 +1,8 @@
 
-using DelimitedFiles
+# Day 1
 
 function solve01()
-  values = readdlm("day01.txt", Int)
+  values = parse.(Int, readlines("day01.txt"))
   for x in values, y in values
     if x + y == 2020 && x <= y
       println("$x * $y = $(x*y)")
@@ -11,7 +11,7 @@ function solve01()
 end
 
 function solve02()
-  values = readdlm("day01.txt", Int)
+  values = parse.(Int, readlines("day01.txt"))
   for x in values, y in values, z in values
     if x + y + z == 2020 && x <= y <= z
       println("$x * $y * $z = $(x*y*z)")
@@ -19,3 +19,40 @@ function solve02()
   end
 end
 
+# Day 2
+
+function solve03()
+  println("Solve task 1 of day 2")
+  lines = readlines("day02.txt")
+  pat = r"^([0-9]+)-([0-9]+) ([a-z]): ([a-z]+)$"
+  valid = filter(lines) do line
+    m = match(pat, line)
+    if isnothing(m)
+      println("Error: did not understand entry '$line'")
+      return false
+    end
+    min, max = parse.(Int, m.captures[1:2])
+    letter = m.captures[3][1]
+    pwd = m.captures[4]
+    min <= count(isequal(letter), pwd) <= max
+  end
+  println("Answer: $(length(valid)) of $(length(lines)) passwords are valid")
+end
+
+function solve04()
+  println("Solve task 2 of day 2")
+  lines = readlines("day02.txt")
+  pat = r"^([0-9]+)-([0-9]+) ([a-z]): ([a-z]+)$"
+  valid = filter(lines) do line
+    m = match(pat, line)
+    if isnothing(m)
+      println("Error: did not understand entry '$line'")
+      return false
+    end
+    min, max = parse.(Int, m.captures[1:2])
+    letter = m.captures[3][1]
+    pwd = m.captures[4]
+    count(isequal(letter),  pwd[[min, max]]) == 1
+  end
+  println("Answer: $(length(valid)) of $(length(lines)) passwords are valid")
+end
